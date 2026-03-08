c269930a33821b4b216c613662d739a49650fbe5
KLAharris <harriskla222@gmail.com>
2026-03-08 21:32:54 +0700

    edit the error icon in cerate discussion page

--- frontend/src/components/create_discussion/Create_discussion_top_bar.jsx
+++ frontend/src/components/create_discussion/Create_discussion_top_bar.jsx
@@ -10,7 +10,7 @@ const Create_discussion_top_bar = () => {
         <div className='w-full bg-white shadow-md py-3'>
             <div className='flex flex-row gap-3 items-center mt-2 mx-4 sm:mx-5 justify-between'>
                 <div className='flex flex-row items-center gap-3'>
-                    <BiChat size={28} className='opacity-70 text-[#3E4A34] hidden sm:block' />
+                    <BiChat size={28} className='opacity-70 text-[#3E4A34]' />
                     <p className='font-["Inter"] text-[18px] sm:text-[32px] text-[#3E4A34] font-thin select-none'>CREATE NEW DISCUSSION</p>
                 </div>
                 <button
